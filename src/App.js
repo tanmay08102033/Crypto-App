47396e7a501881f1d948887d3dc0627210b6ec61
Tanmay <tanmaysingh2203@gmail.com>
2022-12-12 04:50:20 -0800

    Initial

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
+
+function App() {
+    return (<div className="app">
+        <div className="navbar">
+            <Navbar/>
+        </div>
+        <div className="main">
+            <Layout>
+                <div className="routes">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/exchanges" element={<Exchanges/>}/>
+                        <Route path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                        <Route path="/crypto/:coinId" element={<CryptoDetails/>}/>
+                        <Route path="/news" element={<News/>}/>
+                    </Routes>
+                </div>
+            </Layout>
+            <div className="footer">
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Cyrptoverse <br/>
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
+        </div>
+    </div>);
+}
+
+export default App
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (uuid) => createRequest(`/coin/${uuid}`)
+        }),
+        getCryptoHistory: builder.query({
+            query: ({coinId, timePeriod}) => createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/exchanges`),
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery
+} = cryptoApi
